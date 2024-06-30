d491c432b76a782b5ba0ccab46a328ea2578b1fd
gianlucafalaschi <gian100gian@outlook.it>
2024-06-30 17:19:30 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+  export default {
+    components: {
+      
+    }
+  }
+</script>
+
+<template>
+  <h1>Ciao ciao</h1>
+</template>
+
+<style lang="scss">
+@use './style/generic';
+</style>
