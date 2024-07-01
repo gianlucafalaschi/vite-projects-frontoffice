57cee135b848bc5a94657ac3148e6b773c34c60a
gianlucafalaschi <gian100gian@outlook.it>
2024-07-01 07:24:57 +0200

    basic project layout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <script>
+  import ProjectsList from './components/ProjectsList.vue';
+  
   export default {
     components: {
-      
+      ProjectsList,
     }
   }
 </script>
 
 <template>
   <h1>Ciao ciao</h1>
+  <ProjectsList></ProjectsList>
 </template>
 
 <style lang="scss">
 @use './style/generic';
+@use './style/partials/variables' as *;  // le variables vanno importate in tutti ii componenti figli se vengono usate
+
+h1 {
+  color: $main-color;
+}
+
 </style>
---
+++ src/components/ProjectsList.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: 'ProjectsList'
+
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>All projects</h1>
+
+    <!-- Single project template -->
+    <div class="row row-cols-3">
+      <div class="col">
+        <div class="card">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- / Single project template -->
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use '../style/partials/variables' as *;
+</style>
