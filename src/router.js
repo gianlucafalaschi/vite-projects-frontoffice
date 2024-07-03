2392216a083bbaa1d987e48ff0d9bdb00e3652ad
gianlucafalaschi <gian100gian@outlook.it>
2024-07-03 08:58:25 +0200

    vue router scaffolding

--- src/router.js
+++ src/router.js
@@ -1,18 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
-// import AppHome from './pages/AppHome.vue';
-// import PostList from './pages/PostList.vue';
+import AppHome from './pages/AppHome.vue';
+import AppAbout from './pages/AppAbout.vue';
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
-            // path: '/',
-            // name: 'home',
-            // component: AppHome
+            path: '/',
+            name: 'home',
+            component: AppHome
         },
         {
-            // path: '/blog',
-            // name: 'posts',
-            // component: PostList
+            path: '/about',
+            name: 'about',
+            component: AppAbout
         },
     ]
 });
