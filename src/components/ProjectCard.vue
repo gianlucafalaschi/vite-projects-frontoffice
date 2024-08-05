93427511efe1b127415918b79917f566e77134e6
gianlucafalaschi <gian100gian@outlook.it>
2024-08-05 18:28:18 +0200

    add: print technologies

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -16,6 +16,12 @@ export default {
               <strong>Type</strong>:
               {{ projectInfo.type.name}}
             </div>
+            <div v-if="projectInfo.technologies.length > 0">
+              <strong>Technologies</strong>:
+              <div class="d-flex flex-wrap gap-1">
+                <div v-for="technology in projectInfo.technologies" class="badge rounded-pill text-bg-success">{{ technology.name }}</div>
+              </div>
+            </div>
             
             <p v-if="projectInfo.summary" class="card-text">{{ projectInfo.summary }}</p>
             <router-link :to="{name:'single-project', params: {'slug': projectInfo.slug}}" class="btn btn-primary">Details</router-link>
